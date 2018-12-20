81e5a4455df14775b40b067c7071a6ff03eda7e4
dannyrb <danny.ri.brown@gmail.com>
2018-12-20 11:23:24 -0500

    No longer need a different base path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 // https://cli.vuejs.org/config/#vue-config-js
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-    ? '/cornerstonejs-marketplace/'
-    : '/',
     lintOnSave: true
   }
