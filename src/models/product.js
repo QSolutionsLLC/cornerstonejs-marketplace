19d99e53cfe6f63949653e722a106e9035cb7805
dannyrb <danny.ri.brown@gmail.com>
2018-12-20 15:43:05 -0500

    toJson method

--- src/models/product.js
+++ src/models/product.js
@@ -1,26 +1,32 @@
 class Product {
-    constructor(){
-        // 590x300
-        this.id = 0
-        this.imageUrl = ""
-        this.tags = []
-        this.name = ""
-        this.primaryCategory = ""
-        this.categories = []
-        this.author = ""
-        this.lastUpdatedDate = ""
-        this.price = ""
-        this.licenses = []
-        this.description = ""
+  constructor(){
+    // 590x300
+    this.id = 0
+    this.imageUrl = ""
+    this.tags = []
+    this.name = ""
+    this.slug = ""
+    this.primaryCategory = ""
+    this.categories = []
+    this.author = ""
+    this.lastUpdatedDate = ""
+    this.price = ""
+    this.licenses = []
+    this.description = ""
 
-        //
-        this.isFree = false;
-        this.isVerified = false;
-        this.hasFutureUpdates = false;
-        this.includedMonthsSupport = 0;
-        
-        this.authorId = 0
-    }
+    //
+    this.isFree = false;
+    this.isVerified = false;
+    this.hasFutureUpdates = false;
+    this.includedMonthsSupport = 0;
+    
+    this.authorId = 0
+  }
+
+  toJson() {
+    let {id, imageUrl, tags, name, slug, primaryCategory, categories, author, lastUpdatedDate, price, licenses, description, isFree, isVerified, hasFutureUpdates, includedMonthsSupport, authorId} = this;
+    return {id, imageUrl, tags, name, slug, primaryCategory, categories, author, lastUpdatedDate, price, licenses, description, isFree, isVerified, hasFutureUpdates, includedMonthsSupport, authorId};
+  }
 }
 
 export default Product
